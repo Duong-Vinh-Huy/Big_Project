6abfb34216cd2e9f02cc393796498348240120e1
Duong-Vinh-Huy <108985419+Duong-Vinh-Huy@users.noreply.github.com>
2022-08-17 23:36:42 +0700

    index chưa reponsive

--- jsofindex.js
+++ jsofindex.js
@@ -1,5 +1,6 @@
 function init()
     {
+        // scroll chuột
         $(window).scroll(function()
         {
             if($(this).scroll)
@@ -7,5 +8,42 @@ function init()
             else
             $('nav.menu-nav').removeClass('sticky');
         })
+
+        // CLick nút xem tất cả và rút gọn
+        $(".view-all").click(function()
+        {
+            $(".new, .events").css({
+                "overflow":"auto",
+            })
+            $(".rutGon").css({
+                "display":"block",
+            })
+            $(".view-all").css({
+                "display":"none",
+            })
+        })
+        $(".rutGon").click(function()
+        {
+            $(".new, .events").css({
+                "overflow":"hidden",
+            })
+            $(".view-all").css({
+                "display":"block",
+            })
+            $(".rutGon").css({
+                "display":"none",
+            })
+            
+        })
+
+
+        // di chuột vào bản tin
+        // $("div.picture").click(function()
+        // {
+        //     $(".full-info").css({
+        //         "display":"block",
+        //     })
+        // })
+
     }
 
